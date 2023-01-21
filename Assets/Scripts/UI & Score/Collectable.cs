aab49691fb2b7cf86a9879c44ae3b143523caf0d
shreyashap <107754587+shreyashap@users.noreply.github.com>
2023-01-21 15:25:05 +0530

    Added Music to the scene

--- Assets/Scripts/UI & Score/Collectable.cs
+++ Assets/Scripts/UI & Score/Collectable.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     public Score score;
+    private AudioManager aManager;
+ 
 
     private void Start()
     {
-        
+        aManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            aManager.audioSource.PlayOneShot(aManager.collectibleAudio);
             Destroy(this.gameObject);
             score.UpdateScore(5);
         }
     }
+    
 }
